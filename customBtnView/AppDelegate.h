df9706e7f2bd56ca85d9e82d5061a42e4b07ece9
bisheng <517892064@qq.com>
2016-01-07 14:19:37 +0800

    Initial Commit

---
+++ customBtnView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customBtnView
+//
+//  Created by sincere on 16/1/7.
+//  Copyright © 2016年 sincere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customBtnView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customBtnView
+//
+//  Created by sincere on 16/1/7.
+//  Copyright © 2016年 sincere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
